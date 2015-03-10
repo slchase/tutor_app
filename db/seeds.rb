56c87739566b512e1e151e66b83395f03f1062f4
Simons Chase <simons.chase@gmail.com>
2015-03-10 10:58:29 -0400

    adds css

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,6 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 User.destroy_all
 
-User.create(first_name: "Ralph", last_name: "Amen", email: "r@go.co", subjects: "Algebra", zip_code:"10001", password: "1234", password_confirmation: "1234", user_type: "Student")
+User.create(first_name: "Ralph", last_name: "Amen", email: "r@go.co", subjects: "Algebra", zip_code:"10001", password: "1234", password_confirmation: "1234", user_type: "Student", rate_min: "35", rate_max: "50")
 User.create(first_name: "Roger", last_name: "Grace", email: "gg@go.co", subjects: "Calculus", zip_code:"10003", password: "1234", password_confirmation: "1234", user_type: "Tutor")
 User.create(first_name: "Mary", last_name: "Holy", email: "holy@go.co", subjects: "Religion", zip_code:"10005", password: "1234", password_confirmation: "1234", user_type: "Student")
